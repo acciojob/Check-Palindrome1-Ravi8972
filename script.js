7d41bf70486c3c4eee13e27f4b3761e9fdb06e69
Ravi8972 <120507471+Ravi8972@users.noreply.github.com>
2023-06-13 23:56:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // complete the given function
 
 function palindrome(str){
-
+ var re = /[\W_]/g;
+  var lowRegStr = str.toLowerCase().replace(re, '');
+  var reverseStr = lowRegStr.split('').reverse().join(''); 
+  return reverseStr === lowRegStr;
 }
 module.exports = palindrome
